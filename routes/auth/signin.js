3e52dad65bafbd7642b223f61c0e86c2d8a1402c
haraaaaaaa <harunhebibovic05@gmail.com>
2023-08-29 18:23:32 +0200

    signup & signin routes done

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 router.post("/api/users/signin", async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  res.send();
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) return res.status(401).send({ message: "Auth Failed" });
+
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+  if (!isMatch) return res.status(401).send({ message: "Auth Failed" });
+
+  res.status(201).send({
+    message: "auth successful",
+    token: jwt.sign({ id: existingUser._id }, "secret", {
+      expiresIn: "1h",
+    }),
+  });
 });
 
 module.exports = router;
--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const { hash } = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/api/users/signup", async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  res.send();
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(409).send({ message: "Provided E-mail is already in use" });
+
+  const hashedPassword = await hash(password, 10);
+
+  const createdUserDoc = await User.create({
+    email,
+    password: hashedPassword,
+    role: "CEO",
+  });
+
+  res.status(201).send(createdUserDoc);
 });
 
 module.exports = router;
